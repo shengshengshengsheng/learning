43a3bae56f70eb65580a39e6c2a0433031b19222
xuqiangsheng <xuqiangsheng@rfchina.com>
2019-09-19 19:58:33 +0800

    修改

--- eureka_server/src/main/java/com/test/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/test/eureka_server/EurekaServerApplication.java
@@ -3,7 +3,7 @@ package com.test.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//test git ignore
 // 在启动类上添加注解 @EnableEurekaServer 标记这个类是 Eureka服务 启动器
 @EnableEurekaServer
 @SpringBootApplication
